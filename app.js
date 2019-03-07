0213eaf5e52da528abb2b89810d84ed820e1b48b
osoleimani75 <osoleimani75@gmail.com>
2019-03-07 11:16:43 -0800

    add function

--- app.js
+++ app.js
@@ -21,34 +21,8 @@ var monthsWorked = "";
 var monthlyRate = "";
 var totalBilled = "";
 
-$("#submit").on("click", function (event) {
-    // Prevents page refresh
-    event.preventDefault();
-
-    // Capturing values of input fields
-    name = $("#nameInput").val().trim();
-    role = $("#roleInput").val().trim();
-    start = moment().format($("#startInput").val().trim(), "MM/DD/YYYY");
-    rate = $("#rateInput").val().trim();
-    console.log("New Employee added:");
-    console.log(name);
-    console.log(role);
-    console.log(start);
-    console.log(rate);
-    console.log("============================================");
-
-    // Storing input field values in database
-    database.ref().push({
-        name: name,
-        role: role,
-        startDate: start,
-        monthsWorked: moment(start).fromNow(),  // need to somehow save months
-        monthlyRate: rate,
-        totalBilled: parseInt(rate) * parseInt(moment(start).fromNow())
-    })
-
-    // Code in {} runs every time a child is added
-    database.ref().on("child_added", function (snapshot) {
+function showRow (){
+        database.ref().on("child_added", function (snapshot) {
         // Creating elements to display data on page
         var tRow = $("<tr>");
         var nameTD = $("<td>");
@@ -73,5 +47,36 @@ $("#submit").on("click", function (event) {
         tRow.append(totalTD);
         $("tbody").append(tRow);
     });
+}
 
+function addNewRow(){
+    name = $("#nameInput").val().trim();
+    role = $("#roleInput").val().trim();
+    start = moment().format($("#startInput").val().trim(), "MM/DD/YYYY");
+    rate = $("#rateInput").val().trim();
+
+    rateN = isNaN(parseInt(rate)) ? (0) : (parseInt(rate));
+    firstN = isNaN(parseInt(moment(start).fromNow()))? (0): (parseInt(moment(start).fromNow()));
+
+    database.ref().push({
+        name: name,
+        role: role,
+        startDate: start,
+        monthsWorked: moment(start).fromNow(),  // need to somehow save months
+        monthlyRate: rate,
+        totalBilled: rateN * firstN
+    });
+}
+
+
+
+
+
+$( document ).ready(function() {
+    showRow();
+    $("#submit").on("click", function (event) {
+    // Prevents page refresh
+    event.preventDefault();
+    addNewRow();
+});
 });
